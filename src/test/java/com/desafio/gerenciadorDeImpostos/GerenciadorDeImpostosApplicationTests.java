dc51c0d287d8f6053e08d3f4a789b8d66d25a02c
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-05 12:36:36 -0300

    :tada: commit inicial

---
+++ src/test/java/com/desafio/gerenciadorDeImpostos/GerenciadorDeImpostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.gerenciadorDeImpostos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeImpostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
