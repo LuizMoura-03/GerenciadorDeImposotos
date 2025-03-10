e326a47c92db5aa53426cd239f90687bba7b6b02
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-10 15:04:12 -0300

    :sparkles: feat: adiciona interface UserReposiotry e ImpostoRepository

---
+++ src/main/java/com/desafio/gerenciadorDeImpostos/repositories/ImpostoRepository.java
@@ -0,0 +1,9 @@
+package com.desafio.gerenciadorDeImpostos.repositories;
+
+import com.desafio.gerenciadorDeImpostos.models.ImpostoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImpostoRepository extends JpaRepository<ImpostoModel, Long> {
+}
