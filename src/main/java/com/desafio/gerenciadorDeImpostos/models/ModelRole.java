46db5a23605662c6f62735e415a1ebe3ac95750d
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-10 11:37:36 -0300

    :sparkles: feat: adiciona classe ModelRole, com restirçoes de unicidade e não nulo

---
+++ src/main/java/com/desafio/gerenciadorDeImpostos/models/ModelRole.java
@@ -0,0 +1,23 @@
+package com.desafio.gerenciadorDeImpostos.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "roles")
+public class ModelRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
