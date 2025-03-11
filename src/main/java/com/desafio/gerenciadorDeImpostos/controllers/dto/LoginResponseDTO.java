4b87d9c836aaffbb83e4e801fd660ef17101b9f2
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-11 14:05:34 -0300

    :sparkles: feat: criar DTO LoginResponseDTO

---
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/LoginResponseDTO.java
@@ -0,0 +1,10 @@
+package com.desafio.gerenciadorDeImpostos.controllers.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+}
