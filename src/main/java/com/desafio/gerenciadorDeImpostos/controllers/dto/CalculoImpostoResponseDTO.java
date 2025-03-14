fe51aa0c1a4a5062ff1e8a1845257553a1b926e5
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-14 14:40:39 -0300

    :sparkles: feat: adiciona comentarios de validaçoes na classe CalculoImpsotoResponetDTO

--- src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/CalculoImpostoResponseDTO.java
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/CalculoImpostoResponseDTO.java
@@ -1,5 +1,8 @@
 package com.desafio.gerenciadorDeImpostos.controllers.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CalculoImpostoResponseDTO {
+
+    @NotBlank(message = "O nome do imposto é obrigatorio.")
+    private String nomeImposto;
+
+    @NotNull(message = "O valor base é obrigatorio.")
+    @Positive(message = "O valor base deve ser maior que zero.")
     private Double valorBase;
+
+    @NotNull(message = "O valor total do imposto é obrigatorio.")
+    @Positive(message = "O valor total do imposto deve ser maior que zero.")
     private Double aliquota;
+
     private Double valorFixoImposto;
     private Double valorTotalImposto;
 
