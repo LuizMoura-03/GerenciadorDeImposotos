00b5ca706fa5e3e2009bb443d51d6ce130a2d6dc
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-14 14:49:41 -0300

    :sparkles: feat: adiciona comnetarios de validacoes na classe ImpostoResponseDTO

--- src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/ImpostoResponseDTO.java
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/ImpostoResponseDTO.java
@@ -1,5 +1,8 @@
 package com.desafio.gerenciadorDeImpostos.controllers.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +11,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ImpostoResponseDTO {
+
     private Long id;
+
+    @NotBlank(message = "O nome do imposto é obrigatorio.")
     private String name;
+
+    @NotBlank(message = "A descrição do imposto é obrigatoria.")
     private String descricao;
+
+    @Positive(message = "A aliquota do imposto deve ser maior que zero.")
+    @NotNull(message = "A aliquota do imposto é obrigatoria.")
     private double aliquota;
+
+    @Positive(message = "O valor fixo do imposto deve ser maior que zero.")
     private Double valorFixoImposto;
 }
