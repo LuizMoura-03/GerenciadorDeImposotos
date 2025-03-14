e8dbb00f06a2d2d3aadd8650e0c08408f8a6ad91
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-14 13:54:12 -0300

    :sparkles: feat: adiciona classe ImpostoDTO

---
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/ImpostoRequestDTO.java
@@ -0,0 +1,28 @@
+package com.desafio.gerenciadorDeImpostos.controllers.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImpostoRequestDTO {
+
+    @NotBlank(message = "O nome do imposto é obrigatorio.")
+    String name;
+
+    @NotBlank(message = "A descrição do imposto é obrigatória.")
+    private String descricao;
+
+    @Positive(message = "A liquota do imposto tem que ser maior que zero")
+    @NotNull(message = "A aliquota do imposto é obrigatoria")
+    private double aliquota;
+
+    @Positive(message = "A liquota do imposto tem que ser maior que zero")
+    private Double valorFixoImposto;
+
+}
