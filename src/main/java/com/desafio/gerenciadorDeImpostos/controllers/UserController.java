c36ad92cd378ce5cc124fe741c5d37c22597bfd1
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-17 14:18:48 -0300

    :sparkles: feat: adiciona a classe UserController para gerenciamento de usuarios

--- src/main/java/com/desafio/gerenciadorDeImpostos/controllers/UserController.java
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/UserController.java
@@ -1,4 +1,37 @@
 package com.desafio.gerenciadorDeImpostos.controllers;
 
+import com.desafio.gerenciadorDeImpostos.controllers.dto.LoginRequestDTO;
+import com.desafio.gerenciadorDeImpostos.controllers.dto.LoginResponseDTO;
+import com.desafio.gerenciadorDeImpostos.controllers.dto.UserRequestDTO;
+import com.desafio.gerenciadorDeImpostos.controllers.dto.UserResponseDTO;
+import com.desafio.gerenciadorDeImpostos.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@Valid
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO userResponse = userService.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        LoginResponseDTO loginResponse = userService.login(loginRequestDTO);
+        return ResponseEntity.ok().body(loginResponse);
+    }
+
 }
