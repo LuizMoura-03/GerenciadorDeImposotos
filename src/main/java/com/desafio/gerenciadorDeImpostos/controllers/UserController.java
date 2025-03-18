0446ea40e6aa6d2f404080518ec90aac9f56410b
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-18 11:40:09 -0300

    recycle: refactor: corrigindo erro de inicializacao adicionando um construtor manual e removendo @RequiredArgsConstructor

--- src/main/java/com/desafio/gerenciadorDeImpostos/controllers/UserController.java
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/UserController.java
@@ -6,7 +6,6 @@ import com.desafio.gerenciadorDeImpostos.controllers.dto.UserRequestDTO;
 import com.desafio.gerenciadorDeImpostos.controllers.dto.UserResponseDTO;
 import com.desafio.gerenciadorDeImpostos.services.UserService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
-@Valid
 public class UserController {
 
     private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody UserRequestDTO userRequestDTO) {
