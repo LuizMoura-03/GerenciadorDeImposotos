460e1aba8707cfd174ecd2be215afe84aa289900
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-18 11:05:59 -0300

    :sparkles: feat: cria classe InvalidCredentialsException

--- src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/LoginRequestDTO.java
+++ src/main/java/com/desafio/gerenciadorDeImpostos/controllers/dto/LoginRequestDTO.java
@@ -10,4 +10,5 @@ public class LoginRequestDTO {
 
     @NotNull
     private String password;
+
 }
---
+++ src/main/java/com/desafio/gerenciadorDeImpostos/services/AuthenticationServiceImpl.java
@@ -0,0 +1,58 @@
+package com.desafio.gerenciadorDeImpostos.services;
+
+import com.desafio.gerenciadorDeImpostos.controllers.dto.LoginRequestDTO;
+import com.desafio.gerenciadorDeImpostos.controllers.dto.LoginResponseDTO;
+import com.desafio.gerenciadorDeImpostos.exception.InvalidCredentialsException;
+import com.desafio.gerenciadorDeImpostos.infra.jwt.JwtTokenProvider;
+import com.desafio.gerenciadorDeImpostos.models.UserModel;
+import com.desafio.gerenciadorDeImpostos.repositories.UserRepository;
+import com.desafio.gerenciadorDeImpostos.security.CustomUserDetailsService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtTokenProvider jwtTokenProvider, CustomUserDetailsService customUserDetailsService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(AuthenticationServiceImpl.class);
+
+    public LoginResponseDTO authenticate(LoginRequestDTO loginRequestDTO) {
+        logger.info("Tentativa de autenticação para o usuario: {}", loginRequestDTO.getUsername());
+
+        UserModel userModel = userRepository.findByUserName(loginRequestDTO.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+
+        validatePassword(loginRequestDTO.getPassword(), userModel.getPassword());
+
+        String token = generateToken(loginRequestDTO.getUsername());
+
+        logger.info("Autenticação bem-sucedida: {}", loginRequestDTO.getPassword());
+        return new LoginResponseDTO(token);
+    }
+
+    private String generateToken( String username) {
+        return "token-gerado";
+    }
+
+    private void validatePassword(String rawPassword, String encodedPassword) {
+        if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
+            throw new InvalidCredentialsException("Credenciais inválidas!");
+        }
+    }
+}
