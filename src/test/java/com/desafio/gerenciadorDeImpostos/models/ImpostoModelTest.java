72cae1a28d00a59bacde4d7d6794226f509e727a
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-24 19:53:32 -0300

    :test_tube: test: cria testes unitarios ImpostoModelTest para validar restirçoes da entidade ImpostoModel

---
+++ src/test/java/com/desafio/gerenciadorDeImpostos/models/ImpostoModelTest.java
@@ -0,0 +1,44 @@
+package com.desafio.gerenciadorDeImpostos.models;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ImpostoModelTest {
+
+    @Mock
+    private Validator validator;
+    private ImpostoModel imposto;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        imposto = ImpostoModel.builder()
+                .name("Imposto de Renda")
+                .descricao("Imposto sobre a renda")
+                .aliquota(15.0)
+                .valorFixoImposto(100.0)
+                .build();
+    }
+
+    @Test
+    void testValidImpostoModel() {
+
+        Set<ConstraintViolation<ImpostoModel>> violations = validator.validate(imposto); // Valida o objeto imposoto
+
+        assertTrue(violations.isEmpty(), "O modelo de imposto deve ser válido");
+    }
+
+}
