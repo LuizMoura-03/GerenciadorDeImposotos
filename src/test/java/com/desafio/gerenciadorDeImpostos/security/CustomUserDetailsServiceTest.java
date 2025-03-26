a56f6c0a6c9128a290f7ab18132fbb172036bf52
LuizFernando <barbosa.luiz@zup.com.br>
2025-03-25 23:53:45 -0300

    :test_tube: adiciona teste CustomUserDetailsTest

---
+++ src/test/java/com/desafio/gerenciadorDeImpostos/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,65 @@
+package com.desafio.gerenciadorDeImpostos.security;
+
+import com.desafio.gerenciadorDeImpostos.controllers.dto.UserRequestDTO;
+import com.desafio.gerenciadorDeImpostos.controllers.dto.UserResponseDTO;
+import com.desafio.gerenciadorDeImpostos.mappers.UserMapper;
+import com.desafio.gerenciadorDeImpostos.models.RoleModel;
+import com.desafio.gerenciadorDeImpostos.models.UserModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomUserDetailsServiceTest {
+
+    private UserMapper userMapper;
+
+    @BeforeEach
+    void setUp() {
+        userMapper = new UserMapper();
+    }
+
+    @Test
+    void testToEntity() {
+        UserRequestDTO userRequestDTO = new UserRequestDTO();
+        userRequestDTO.setName("Test User");
+        String encodedPassword = "encodedPassword123";
+        RoleModel role = new RoleModel();
+        role.setName("ROLE_USER");
+        Set<RoleModel> roles = Set.of(role);
+
+        UserModel userModel = userMapper.toEntity(userRequestDTO, encodedPassword, roles);
+
+        assertNotNull(userModel);
+        assertEquals("Test User", userModel.getName());
+        assertEquals("encodedPassword123", userModel.getPassword());
+        assertEquals(1, userModel.getRoles().size());
+        assertTrue(userModel.getRoles().stream().anyMatch(r -> r.getName().equals("ROLE_USER")));
+    }
+
+    @Test
+    void testToResponse() {
+        RoleModel role = new RoleModel();
+        role.setName("ROLE_USER");
+        Set<RoleModel> roles = Set.of(role);
+
+        UserModel userModel = UserModel.builder()
+                .id(1L)
+                .name("Test User")
+                .password("encodedPassword123")
+                .roles(roles)
+                .build();
+
+
+        UserResponseDTO userResponseDTO = userMapper.toResponse(userModel);
+
+        assertNotNull(userResponseDTO);
+        assertEquals(1L, userResponseDTO.getId());
+        assertEquals("Test User", userResponseDTO.getName());
+        assertEquals(1, userResponseDTO.getRoles().size());
+        assertTrue(userResponseDTO.getRoles().contains("ROLE_USER"));
+    }
+
+}
